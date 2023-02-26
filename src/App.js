82b6d477f68d10d75f2064777a2593cf2d3b566f
Yogendra Kumar Dubey <93875323+ykd27@users.noreply.github.com>
2023-02-26 13:37:29 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import './App.css';
+import SearchMain from './components/SearchMain';
+
+
+export default function App() {
+  return (
+    <div className='App'>
+    <SearchMain/>
+    </div>
+  )
+}
+
