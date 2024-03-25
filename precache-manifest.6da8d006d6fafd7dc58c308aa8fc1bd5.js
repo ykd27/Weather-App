272d5370672db548a98e462709b6209873f7663f
Yogendra Kumar Dubey <ykdykd27@gmail.com>
2024-03-25 12:00:30 +0530

    Updates

---
+++ precache-manifest.6da8d006d6fafd7dc58c308aa8fc1bd5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f84ea46a5f1660684213",
+    "url": "/Weather-App/static/css/main.2a4310ff.chunk.css"
+  },
+  {
+    "revision": "f84ea46a5f1660684213",
+    "url": "/Weather-App/static/js/main.f84ea46a.chunk.js"
+  },
+  {
+    "revision": "fc1e816dc451fc01e207",
+    "url": "/Weather-App/static/js/1.fc1e816d.chunk.js"
+  },
+  {
+    "revision": "edcfa8a852f959e2feef",
+    "url": "/Weather-App/static/js/runtime~main.edcfa8a8.js"
+  },
+  {
+    "revision": "5d74aec8f989fe1b43d38057fc03afab",
+    "url": "/Weather-App/index.html"
+  }
+];
